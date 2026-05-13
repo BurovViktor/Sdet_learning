9cf533ac50b29e340ed37efbbaa93189238fd295
palam - s.kabanov@webinar.ru <s.kabanov@webinar.ru>
2026-05-13 14:49:07 +0300

    Добавил класс в котором открывается сайт курорта Красная Пахра

---
+++ src/test/java/web/yandextest/OpenHotelKrasnayaPahra.java
@@ -0,0 +1,20 @@
+package web.yandextest;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class OpenHotelKrasnayaPahra {
+    @Test
+    public void userCanLoginByUsername() {
+        open("https://krasnaya-pakhra-kurort.ru/");
+        Selenide.switchTo().frame("booking-search-frame");
+        $(".rec-dates-btn").scrollIntoCenter().click();
+       // $$("#ares li.g").shouldHave(CollectionCondition.size(10));
+       // $("#ares li.g").shouldHave(text("Яндекс Карты — транспорт, навигация, поиск мест"));
+        System.out.println("");
+    }
+
+}
