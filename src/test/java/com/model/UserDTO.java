3adab8df6054038e6b4b9c7610b27aff3ca285ca
palam - s.kabanov@webinar.ru <s.kabanov@webinar.ru>
2026-06-23 19:56:08 +0300

    Добавлено тело к прошлым тестам.

--- src/test/java/com/model/UserDTO.java
+++ src/test/java/com/model/UserDTO.java
@@ -1,8 +1,18 @@
 package com.model;
 
+import net.bytebuddy.asm.Advice;
+
 public class UserDTO {
-    public String name;
-    public String job;
+    private String name;
+    private String job;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
 
     public UserDTO(String name, String job) {
         this.name = name;
