a9d7ba625be0eb90e4b42002aa15f67e3d5e7770
palam - s.kabanov@webinar.ru <s.kabanov@webinar.ru>
2026-07-17 21:40:55 +0300

    Добавлены методы нажатия и наведения на навигационную панель

---
+++ src/main/java/krasnayaPahra/ui/pageObjects/fiow/MainPageFlow.java
@@ -0,0 +1,36 @@
+package krasnayaPahra.ui.pageObjects.fiow;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import krasnayaPahra.ui.pageObjects.BaseFlow;
+import krasnayaPahra.ui.pageObjects.pages.MainPage;
+import org.junit.jupiter.api.DisplayName;
+
+import static com.codeborne.selenide.Selenide.open;
+import static krasnayaPahra.ui.pageObjects.enums.EnamsPahra.BASE_URL;
+
+public class MainPageFlow  implements BaseFlow {
+
+
+    private final MainPage mainPageElements = new MainPage();
+
+    @DisplayName("Открыть главную страницу в браузере")
+    public void openMainPageInBrowser() {
+        open(BASE_URL.toString());
+    }
+
+    @DisplayName("Открыта главная страница")
+    public void mainPageIsLoaded() {
+        mainPageElements.getMainElement().shouldBe(Condition.clickable);
+    }
+
+    @DisplayName("Переключится на BookingIframe")
+    public void switchToBookingIframe() {
+        Selenide.switchTo().frame(mainPageElements.getBookingIFrameElement());
+    }
+
+    @DisplayName("Перейти на флоу навигации")
+    public NavigationHeaderPanelFlaw atNavigationFlow() {
+        return new NavigationHeaderPanelFlaw();
+    }
+}
