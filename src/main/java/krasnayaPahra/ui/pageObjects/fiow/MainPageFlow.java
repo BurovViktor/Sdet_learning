b5b3ae2c6cab9aceba69b6839a82380b23469b7f
palam - s.kabanov@webinar.ru <s.kabanov@webinar.ru>
2026-07-21 19:38:51 +0300

    Добавлены методы с вызовом классов BusinessHeaderFlow и NomeraBusinessFlow

--- src/main/java/krasnayaPahra/ui/pageObjects/fiow/MainPageFlow.java
+++ src/main/java/krasnayaPahra/ui/pageObjects/fiow/MainPageFlow.java
@@ -3,13 +3,15 @@ package krasnayaPahra.ui.pageObjects.fiow;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import krasnayaPahra.ui.pageObjects.BaseFlow;
+import krasnayaPahra.ui.pageObjects.pages.BusinessPage;
 import krasnayaPahra.ui.pageObjects.pages.MainPage;
 import org.junit.jupiter.api.DisplayName;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static krasnayaPahra.ui.pageObjects.enums.EnamsPahra.BASE_URL;
 
-public class MainPageFlow  implements BaseFlow {
+public class MainPageFlow implements BaseFlow {
 
 
     private final MainPage mainPageElements = new MainPage();
@@ -33,4 +35,11 @@ public class MainPageFlow  implements BaseFlow {
     public NavigationHeaderPanelFlaw atNavigationFlow() {
         return new NavigationHeaderPanelFlaw();
     }
+    public BusinessHeaderFlow atBusinessHeaderFlow() {
+        return new BusinessHeaderFlow();
+    }
+    public NomeraBusinessFlow atNomeraBusinessFlow(){
+        return new NomeraBusinessFlow();
+    }
+
 }
