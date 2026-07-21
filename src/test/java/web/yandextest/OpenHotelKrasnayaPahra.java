a01a1bf26dda794f4fa24e86e451c366534d5de7
palam - s.kabanov@webinar.ru <s.kabanov@webinar.ru>
2026-07-21 19:43:56 +0300

    Добавлены тесты с кликами по вкладкам и написанием текста в строку ФИО

--- src/test/java/web/yandextest/OpenHotelKrasnayaPahra.java
+++ src/test/java/web/yandextest/OpenHotelKrasnayaPahra.java
@@ -1,16 +1,21 @@
 package web.yandextest;
 
 import krasnayaPahra.ui.pageObjects.fiow.MainPageFlow;
+import krasnayaPahra.ui.pageObjects.fiow.RestaurantHeaderFlow;
+import krasnayaPahra.ui.pageObjects.fiow.RoomsHeaderFlow;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
 
 
 public class OpenHotelKrasnayaPahra {
 
     private MainPageFlow mainPageFlow;
 
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         mainPageFlow = new MainPageFlow();
 
         mainPageFlow.openMainPageInBrowser();
@@ -31,8 +36,9 @@ public class OpenHotelKrasnayaPahra {
 
         mainPageFlow.switchToBookingIframe();
     }
+
     @Test
-    void hoverOnNavigationPanel(){
+    void hoverOnNavigationPanel() {
         mainPageFlow.atNavigationFlow()
                 .hoverOnPrices();
         mainPageFlow.atNavigationFlow().waitSec(3);
@@ -41,4 +47,53 @@ public class OpenHotelKrasnayaPahra {
                 hoverOnRestaurants();
         mainPageFlow.atNavigationFlow().waitSec(3);
     }
+
+    @Test
+    void hoverOnRestaurantPanel() {
+        mainPageFlow.atNavigationFlow().hoverOnRestaurants();
+        mainPageFlow.atNavigationFlow()
+                .atRestaurantHeaderFlow()
+                .clickRestaurant();
+        //   System.out.println("...");
+    }
+
+    @Test
+    void hoverOnLobbyBar() {
+        mainPageFlow.atNavigationFlow().hoverOnRestaurants();
+        mainPageFlow.atNavigationFlow()
+                .atRestaurantHeaderFlow()
+                .clickLobbyBar();
+
+    }
+
+    @Test
+    void hoverOnStudio() {
+        mainPageFlow.atNavigationFlow().hoverOnRooms();
+        mainPageFlow.atNavigationFlow()
+                .atRoomsHeaderFlow()
+                .clickBusiness();
+    }
+
+    @Test
+    void scrollBusiness() {
+        mainPageFlow.atNavigationFlow().hoverOnRooms();
+        mainPageFlow.atNavigationFlow()
+                .atRoomsHeaderFlow()
+                .clickBusiness();
+        mainPageFlow.atBusinessHeaderFlow().scroll();
+
+    //    System.out.println("...");
+    }
+    @Test
+    void writeFoolNameInBooking() {
+        mainPageFlow.atNavigationFlow().hoverOnRestaurants();
+        mainPageFlow.atNavigationFlow()
+                .atRestaurantHeaderFlow()
+                .clickLobbyBar();
+        mainPageFlow.atNomeraBusinessFlow().fullNameInBooking();
+
+        System.out.println("...");
+
+    }
 }
+
